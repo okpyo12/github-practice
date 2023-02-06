524bdfc9eff748ee4571300eb1b4f7c2e3d904a2
Pyosangwoo <psw3619@naver.com>
2023-02-06 17:05:36 +0900

    feat. branch 실습 코드

---
+++ pages/test1.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Test1: NextPage = () => {
+  return <div>test1</div>;
+};
+
+export default Test1;
---
+++ pages/test2.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Test2: NextPage = () => {
+  return <div>test2</div>;
+};
+
+export default Test2;
